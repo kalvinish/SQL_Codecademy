9b1f8db835b0bd8300527f0cd480fc309c615251
Kalvin <kalvinedwardroberts@gmail.com>
2020-08-26 21:26:21 +0100

    second commit with sql project 1

---
+++ SQL_Project_1.sql
@@ -0,0 +1,28 @@
+-- This is the first query:
+
+SELECT DISTINCT year from population_years;
+
+-- Add your additional queries below:
+
+SELECT population FROM population_years
+WHERE country = 'Gabon'
+ORDER BY population DESC
+LIMIT 1;
+
+SELECT country, population FROM population_years
+WHERE year = 2005
+ORDER BY population ASC
+LIMIT 10;
+
+SELECT DISTINCT country FROM population_years
+WHERE year = 2010
+	AND population > 100;
+
+SELECT count(*) FROM population_years
+WHERE country LIKE '%Islands%';
+
+SELECT * FROM population_years
+WHERE country = 'Indonesia'
+	AND year = 2000
+OR country = 'Indonesia'
+	AND year = 2010;
